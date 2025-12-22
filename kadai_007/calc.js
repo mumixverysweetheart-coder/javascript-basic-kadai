90bb17cf9e4c39861432241f3ff2b037e1aef9f7
mumixverysweetheart-coder <mu.mix.very.sweet.heart@gmail.com>
2025-12-22 19:55:39 +0900

    kadai_007を追加

---
+++ kadai_007/calc.js
@@ -0,0 +1,10 @@
+//変数に1〜100の整数を代入する
+for (let i = 1 ; i <= 100 ; i++){
+
+if (i % 3 === 0 &&  i % 5 ===0 ){console.log('3と5の倍数です');}
+ 
+ else if (i % 3 === 0 ){console.log('3の倍数です');}
+
+ else if (i % 5 === 0 && i % 5 === 0 ){console.log('5の倍数です');}
+
+ else {console.log(i);}}
